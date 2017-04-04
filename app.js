eb83518fcdf22b78d71518629f588d69bcb37a19
yasuper8 <ysuper8@gmail.com>
2017-04-04 16:47:13 -0700

    add dependencies for passport user auth

--- app.js
+++ app.js
@@ -1,10 +1,50 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const multer = require('multer');
+const flash = require('connect-flash');
+
 const routes = require('./routes/routes');
+
 const app = express();
 
 app.use(bodyParser.json());
 routes(app);
+// Middleware///
+// Handle file uploads
+app.use(multer({dest: './uploads'}));
+
+// Handle Sessions
+app.use(session({
+  seacret: 'seacret',
+  saveUninitialized: true,
+  resave: true;
+}));
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
 
 module.exports = app;
